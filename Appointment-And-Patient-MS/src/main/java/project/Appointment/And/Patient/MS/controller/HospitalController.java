a1812a8835e520d2e5f3aab4604ac6a214e72c88
joshua <joshuasirpatrickmumba@gmail.com>
2024-12-16 23:15:09 +0300

    updates

--- Appointment-And-Patient-MS/src/main/java/project/Appointment/And/Patient/MS/controller/HospitalController.java
+++ Appointment-And-Patient-MS/src/main/java/project/Appointment/And/Patient/MS/controller/HospitalController.java
@@ -2,6 +2,7 @@ package project.Appointment.And.Patient.MS.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import project.Appointment.And.Patient.MS.model.Hospital;
 import project.Appointment.And.Patient.MS.service.HospitalService;
